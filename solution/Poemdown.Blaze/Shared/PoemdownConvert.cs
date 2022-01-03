126a672d524acd40eaabcafaa91daeea87cabe9d
connectuum <connectuum@pm.me>
2022-01-03 14:29:07 -0500

    improved the overlay HTML and styles
    the overlay colors are now more vibrant, while the markdown symbols are more subdued. the goal is for the control characters to be present, readable, editable, but _not_ outshining or distracting from the words. this is the editor panel, so we need to see everything clearly -- but not necessarily at equal weighting.
    
    also, we now use custom HTML tags (like `head1`) in place of `span` tags with  `class` specifications. not a vital change, but it looks cleaner in the code, and it avoids the need for the `\v` special case.

--- solution/Poemdown.Blaze/Shared/PoemdownConvert.cs
+++ solution/Poemdown.Blaze/Shared/PoemdownConvert.cs
@@ -17,13 +17,13 @@ public static class PoemdownConvert {
 	public static string ToOverlayHtml(in string poemdown) {
 		string html = poemdown;
 		html = Regex.Replace(html, @"\r", "");
-		html = Regex.Replace(html, @"(\*[^\*\n]+\*)", "<b>$1</b>");
-		html = Regex.Replace(html, @"(_[^_\n]+_)", "<em>$1</em>");
-		html = Regex.Replace(html, @"(`[^_\n]+`)", "<span\vclass='code'>$1</span>");
-		html = Regex.Replace(html, @"(^|\n)(---)($|\n)", "$1<span\vclass='line'>$2</span><hr/>");
-		html = Regex.Replace(html, @"(^|\n)(#\s+[^$\n]+)", "$1<span\vclass='head1'>$2</span>");
-		html = Regex.Replace(html, @"(^|\n)(##\s+[^$\n]+)", "$1<span\vclass='head2'>$2</span>");
-		html = Regex.Replace(html, @"(^|\n)(###\s+[^$\n]+)", "$1<span\vclass='head3'>$2</span>");
+		html = Regex.Replace(html, @"(\*)([^\*\n]+)(\*)", "<b><c>$1</c>$2<c>$3</c></b>");
+		html = Regex.Replace(html, @"(_)([^_\n]+)(_)", "<em><c>$1</c>$2<c>$3</c></em>");
+		html = Regex.Replace(html, @"(`)([^_\n]+)(`)", "<mono><c>$1</c>$2<c>$3</c></mono>");
+		html = Regex.Replace(html, @"(^|\n)(---)($|\n)", "$1<line><c>$2</c></line><hr/>");
+		html = Regex.Replace(html, @"(^|\n)(#)(\s+[^$\n]+)", "$1<head1><c>$2</c>$3</head1>");
+		html = Regex.Replace(html, @"(^|\n)(##)(\s+[^$\n]+)", "$1<head2><c>$2</c>$3</head2>");
+		html = Regex.Replace(html, @"(^|\n)(###)(\s+[^$\n]+)", "$1<head3><c>$2</c>$3</head3>");
 
 		var htmlBuild = new StringBuilder();
 		int lineCharCount = 0;
